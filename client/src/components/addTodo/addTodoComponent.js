5efd33f3df973c94ec03f6b71688234ce90d8f25
dbacall <dbacall@hotmail.co.uk>
2021-02-12 11:25:29 +0000

    add form inputs

--- client/src/components/addTodo/addTodoComponent.js
+++ client/src/components/addTodo/addTodoComponent.js
@@ -3,12 +3,32 @@ import styles from './addTodo.module.scss';
 
 const AddTodo = () => {
   const [openForm, setOpenForm] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [dateToCompleteBy, setDateToCompleteBy] = useState('');
 
   const renderForm = () => {
     if (openForm) {
       return (
         <form>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            type="date"
+            placeholder="Date to complete"
+            value={dateToCompleteBy}
+            onChange={(e) => setDateToCompleteBy(e.target.value)}
+          />
         </form>
       );
     }
