1f4ee5f52ae92a9752f50dee16a36804305b5439
dbacall <dbacall@hotmail.co.uk>
2021-02-12 09:40:53 +0000

    add database loader

---
+++ server/src/loaders/database.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  // set up database connection
+
+  const url = process.env.DB_URL;
+
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log(
+        `Successfully connected to ${process.env.NODE_ENV} mongoDB database.`
+      );
+    })
+    .catch((err) => console.log(err));
+};
