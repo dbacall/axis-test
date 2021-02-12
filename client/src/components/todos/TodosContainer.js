85ebadf04e0c7660e787b8a344d84cefcce89a39
dbacall <dbacall@hotmail.co.uk>
2021-02-12 12:00:37 +0000

    get todos from database in useEffect and send to component

--- client/src/components/todos/TodosContainer.js
+++ client/src/components/todos/TodosContainer.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Todos from './TodosComponent';
 import api from '../../services/api';
 
 const TodosContainer = ({ todoAdded }) => {
-  return <Todos todoAdded={todoAdded} />;
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+
+      const path = '/todo';
+
+      const response = await api.request({ method: 'get', path });
+
+      setTodos(response.data);
+      setLoading(false);
+    })();
+  }, [todoAdded]);
+  return <Todos todoAdded={todoAdded} loading={loading} todos={todos} />;
 };
 
 export default TodosContainer;
