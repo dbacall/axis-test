571daacdddd051a50f0d3e1c0c450cf6a6c10108
dbacall <dbacall@hotmail.co.uk>
2021-02-12 11:51:23 +0000

    change classNames

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import styles from './App.module.scss';
+import React from 'react';
+import './App.css';
 import AddTodo from './components/addTodo/addTodoContainer';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
+    <div className="App">
+      <header className="AppHeader">
         <h2>Axis Todo</h2>
       </header>
       <AddTodo />
